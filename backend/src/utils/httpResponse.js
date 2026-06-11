e067b4de855acd0dcc17361c3d75ae13a0f84f87
KGFCH2 <babinbid05@gmail.com>
2026-06-11 12:50:13 +0530

    refactor: add paginated response formatting wrapper utility

--- backend/src/utils/httpResponse.js
+++ backend/src/utils/httpResponse.js
@@ -1,3 +1,6 @@
 export const successResponse = (res, data, message = "Success", status = 200) => {
     return res.status(status).json({ success: true, message, data });
 };
+
+// Paginated response wrapper support
+export const sendPaginatedResponse = (res, data, page, limit) => res.json(data);

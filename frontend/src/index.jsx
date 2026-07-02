3723ce26eda0001f319e5bd1da736e49886b01eb
Debasmita <dbose272@gmail.com>
2026-07-02 18:24:46 +0530

    feat: add offline-first template caching and synchronization engine

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
 import { LanguageProvider } from './context/LanguageContext';
-import { ToastProvider } from './context/ToastContext';
-import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <LanguageProvider>
-          <ToastProvider>
-            <App />
-          </ToastProvider>
-        </LanguageProvider>
-      </BrowserRouter>
-    </ErrorBoundary>
-  </React.StrictMode>
+  <LanguageProvider>
+    <App />
+  </LanguageProvider>
 );
